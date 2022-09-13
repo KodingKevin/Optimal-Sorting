c1d75ef22cd6759405894a04a531013c6039b116
Avanish Jeendru <avanish.jeendru@student.allenisd.org>
2022-09-12 22:34:48 -0500

    V1

--- main2.swift
+++ main2.swift
@@ -1,11 +1,11 @@
-let words = ["bark", "shallow", "box", "apple", "dragon", "sack", "armadillo", "aardavark"]
+let words = ["bark", "shallow", "box", "apple", "dragon", "sack", "armadillo", "aardavark", "alligator", "ark"]
 
 let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var done : [String] = []
 
-var index = 0
-var index2 = 0
+//var index = 0
+//var index2 = 0
 
 for letter in letters {
     var sorted : [String] = []
@@ -13,15 +13,32 @@ for letter in letters {
         if String(word.first!) == String(letter) {
             sorted.append(word)
         }        
-        index += 1
     }
+//Checks 2nd letter
     for letter in letters {
+        var sorted2 : [String] = []
         for word in sorted {
-            let char2 = Array(word)[1]   
-            if String(char2) == String(letter) {
-                done.append(word)
+            if word.count > 1 {
+                let char2 = Array(word)[1]   
+                if String(char2) == String(letter) {
+                    sorted2.append(word)
+                }
+            } else {
+                sorted2.append(word)
             }
-            index2 += 1
+        }
+//Checks 3rd letter
+        for letter in letters {
+            for word in sorted2 {
+                if word.count > 2 {
+                    let char3 = Array(word)[2]   
+                    if String(char3) == String(letter ) {
+                        done.append(word)
+                    }
+                } else {
+                    done.append(word)
+                }
+            }
         }
     }
 }
